1631e061d7a07ee8cb460a1a51b06955cf85d720
amitsh0 <amit.shain.2000@gmail.com>
2024-05-20 16:43:37 +0300

    Added provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import { Layout } from './Components/LayoutArea/Layout/Layout';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <Layout/>
+    <Provider store = {store}>
+        <Layout/>
+    </Provider> 
   </BrowserRouter>
 );
 
